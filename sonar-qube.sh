321536172a647b432edea75125f845c53a7c32a3
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2019-02-17 23:54:21 +0300

    Настройка sonar

--- sonar-qube.sh
+++ sonar-qube.sh
@@ -1,27 +1,18 @@
+#!/bin/bash
+
+oscript ./tasks/coverage.os
+
 temp=`cat packagedef | grep ".Версия(" | sed 's|[^"]*"||' | sed -r 's/".+//'`
 version=${temp##*|}
-if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    wget -O bsl-language-server.jar https://github.com/1c-syntax/bsl-language-server/releases/download/v0.3.0/bsl-language-server-0.3.0.jar
-    java -jar ./bsl-language-server.jar -a -s ./src -r json
-    
-    sonar-scanner \
-        -Dsonar.host.url=https://sonar.silverbulleters.org \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_OAUTH_TOKEN \
-        -Dsonar.login=$SONAR_TOKEN \
-        -Dsonar.scanner.skip=false
 
-  elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    wget -O bsl-language-server.jar https://github.com/1c-syntax/bsl-language-server/releases/download/v0.3.0/bsl-language-server-0.3.0.jar
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     java -jar ./bsl-language-server.jar -a -s ./src -r json
+
     sonar-scanner \
-        -Dsonar.host.url=https://sonar.oscript.ru \
+        -Dsonar.host.url=$SONAR_HOST \
         -Dsonar.login=$SONAR_TOKEN \
         -Dsonar.projectVersion=$version\
         -Dsonar.scanner.skip=false
   fi
 fi
-
