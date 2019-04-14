552eb282e87f5dce882e3bd538c8967f554689a4
Nikita Gryzlov <nixel2007@gmail.com>
2019-04-14 13:22:38 +0300

    Актуализация алгоритма анализа

--- sonar-qube.sh
+++ sonar-qube.sh
@@ -7,8 +7,6 @@ version=${temp##*|}
 
 if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
   if [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    wget -O bsl-language-server.jar https://github.com/1c-syntax/bsl-language-server/releases/download/v0.3.0/bsl-language-server-0.3.0.jar
-    java -jar ./bsl-language-server.jar -a -s ./src -r json
 
     sonar-scanner \
         -Dsonar.host.url=$SONAR_HOST \
